cedd05021a02833391b28b743f06570f2abaeb5c
Pakrada <s6302041510079@email.kmutnb.ac.th>
2021-09-16 17:31:10 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mobileappweek1/config/constant.dart';
+import 'package:mobileappweek1/screen/login.dart';
+import 'package:mobileappweek1/screen/register.dart';
+import 'screen/index.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Index(),
+      routes: {
+        'Login': (context) => Login(),
+        'Register': (context) => Register(),
+      },
+      theme: ThemeData(
+        primaryColor: sColor,
+        secondaryHeaderColor: sColor,
+      ),
+    );
+  }
+}
